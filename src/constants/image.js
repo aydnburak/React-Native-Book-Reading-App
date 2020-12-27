d9a9d44eba0699da358a1fb001309636f4acbfb8
aydnburak <burakaydin9761@gmail.com>
2020-12-27 18:33:13 +0300

    Verileri Ekledim

---
+++ src/constants/image.js
@@ -0,0 +1,11 @@
+const IMAGE = {
+    ICON_HOME: require('../images/home.png'),
+    ICON_HOME2: require('../images/home2.png'),
+    ICON_SEARCH: require('../images/search.png'),
+    ICON_SEARCH2: require('../images/search2.png'),
+    ICON_USER: require('../images/user.png'),
+    ICON_USER2: require('../images/user2.png'),
+    ICON_BACK: require('../images/back.png'),
+    ICON_MENU: require('../images/menu.png'),
+}
+export { IMAGE }
---
+++ src/constants/kitap.js
@@ -0,0 +1,40 @@
+const KİTAP = {
+    ICERİK: "Çocukluğu v e ailesi: Rus edebiy atının en büy ük kişilerinden biri olan Fiy odor Mihay lov iç Dostoy ev ski,"+
+    "1 821 y ılı kasım ay ının 1 1 inde Moskov a’da,"+
+    "y oksullara mahsus bir hastanede düny ay a geldi."+
+    "Küçük Dostoy ev ski’nin ilk izlenimleri, hastanenin sarı"+
+    "duv arlarıy la hastaların iniltileri oldu."+
+    "Ay nı müessesenin cerrahı olan babası, alkolik, sert"+
+    "v e sinirli bir adamdı. İşinde, ukalâ denecek kadar"+
+    "düzeni sev erdi. Ailesi kalabalık, maaşı azdı. Ama y ine"+
+    "de çocuklarının tahsiline büy ük bir ilgi gösterdi."+
+    "Fransızca öğrenmeleri için bir mürebbiy e tuttu."+
+    "Kendisi de onlara Lâtince öğretti."+
+    "Dostoy ev ski on üç y aşınday ken, Moskov a’nın en"+
+    "iy i özel okullarından birine y atılı olarak v erildi. 1 837"+
+    "y ılında da, henüz 1 6 y aşında bir delikanlıy ken,"+
+    "babasının zoruy la Petersburg askerî mühendis"+
+    "okuluna girdi. Ay nı y ıl içinde annesini kay betti."+
+    "Dostoy ev ski’nin babası, 1 839 y ılında kendi toprak"+
+    "köleleri tarafından öldürüldü. Bu kanlı olay ın nedeni,"+
+    "son y ıllara kadar bir esrar perdesiy le örtülüy dü. Tam"+
+    "y üzy ıl sonra, y ani 1 939 y ılında büy ük y azarın"+
+    "hay atına ışık tutan y eni bazı v esikalar bulundu. Bu"+
+    "v e sinirli bir adamdı. İşinde, ukalâ denecek kadar"+
+    "düzeni sev erdi. Ailesi kalabalık, maaşı azdı. Ama y ine"+
+    "de çocuklarının tahsiline büy ük bir ilgi gösterdi."+
+    "Fransızca öğrenmeleri için bir mürebbiy e tuttu."+
+    "Kendisi de onlara Lâtince öğretti."+
+    "Dostoy ev ski on üç y aşınday ken, Moskov a’nın en"+
+    "iy i özel okullarından birine y atılı olarak v erildi. 1 837"+
+    "y ılında da, henüz 1 6 y aşında bir delikanlıy ken,"+
+    "babasının zoruy la Petersburg askerî mühendis"+
+    "okuluna girdi. Ay nı y ıl içinde annesini kay betti."+
+    "Dostoy ev ski’nin babası, 1 839 y ılında kendi toprak"+
+    "köleleri tarafından öldürüldü. Bu kanlı olay ın nedeni,"+
+    "son y ıllara kadar bir esrar perdesiy le örtülüy dü. Tam"+
+    "y üzy ıl sonra, y ani 1 939 y ılında büy ük y azarın"+
+    "hay atına ışık tutan y eni bazı v esikalar bulundu. Bu"
+    
+}
+export { KİTAP }
